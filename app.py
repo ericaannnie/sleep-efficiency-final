7a3a97a06241923eadcd78e5935d2b0b4b4ba499
jfarias123 <117544541+jfarias123@users.noreply.github.com>
2023-12-03 18:28:11 +0100

    Update app.py

--- app.py
+++ app.py
@@ -505,6 +505,11 @@ if app_mode == 'Conclusion':
             "https://media.tenor.com/jQlbcSS2HgoAAAAd/tom-and-jerry-sleep.gif",
             width=400,
         )
-     st.markdown("Increase sleep duration to have the biggest positive impact on level of sleep quality. ")
-
-
+    if app_mode == 'Conclusion':
+    # Display dataset details
+    st.markdown("### Key Takeaways")
+    st.image(
+            "https://media.tenor.com/jQlbcSS2HgoAAAAd/tom-and-jerry-sleep.gif",
+            width=400,
+        )
+    st.markdown("Increase sleep duration to have the biggest positive impact on level of sleep quality. ")
