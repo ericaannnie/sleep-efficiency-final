ae784de47d426ce6d01b904739b82e039d46b2d7
ericaannnie <147415296+ericaannnie@users.noreply.github.com>
2023-12-04 14:01:12 +0100

    Update app.py

--- app.py
+++ app.py
@@ -234,10 +234,10 @@ if app_mode == 'Introduction':
         
         lm,X_train,y_test,predictions,model = predict(select_variable,test_size,pred_df,output_multi)
 
-    # Stop tracking and get emissions
-    emissions = 100
+# Stop tracking and get emissions
+emissions = 100
 
-    print(f"Total emissions: {emissions} kg")
+print(f"Total emissions: {emissions} kg")
     
     
     
