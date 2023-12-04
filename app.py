a7c811e9a6dcb8fa814430cc13e3e2c6b65f4e0f
Reynaldo <57570254+Olympian13@users.noreply.github.com>
2023-12-04 01:39:15 +0100

    Update app.py

--- app.py
+++ app.py
@@ -37,7 +37,7 @@ from sklearn.metrics import mean_squared_error, r2_score
 
 MODELS = {
     "Linear Regression": LinearRegression, 
-    "Random Forest': RandomForestRegressor
+    "Random Forest": RandomForestRegressor
 }
 
 
