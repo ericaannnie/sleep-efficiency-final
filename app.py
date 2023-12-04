9a648a26daf9876c74cd980c217fad7e3969bae8
Reynaldo <57570254+Olympian13@users.noreply.github.com>
2023-12-04 12:23:22 +0100

    Update app.py

--- app.py
+++ app.py
@@ -493,7 +493,7 @@ if app_mode == 'Prediction':
         #y_test = np.argmax(y_test)
         #predictions=np.argmax(predictions)
         y_test = y_test..to_numpy()
-        predictions = predictions..to_numpy()
+        predictions = predictions.to_numpy()
         acc = accuracy_score(y_test, predictions)
         st.write("1) Model Accuracy (in %):", np.round(acc*100,2))
         f1_score = f1_score(y_test, predictions, average='weighted')
